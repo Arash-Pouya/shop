90f1a069bdbfd48517e75f1b4cbe2cc1bd0307b2
Arash-pouya <arash.pouya.c@gmail.com>
2021-06-20 13:56:05 +0430

    msater

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $(".card")
+    .delay(1800)
+    .queue(function (next) {
+      $(this).removeClass("hover");
+      $("a.hover").removeClass("hover");
+      next();
+    });
+});
+/* ///////////details page///////// */
+
+/* ///////////details page///////// */
