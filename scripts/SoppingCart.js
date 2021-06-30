d29bde2db605b02150ad41599ae1ec53ca142898
Arash-pouya <arash.pouya.c@gmail.com>
2021-07-01 01:05:40 +0430

    master

--- scripts/SoppingCart.js
+++ scripts/SoppingCart.js
@@ -122,11 +122,16 @@ function removeItem(removeButton) {
     updateSumItems();
   });
 }
-
 anime({
   targets: ".div",
   translateX: 200,
   direction: "alternate",
   loop: true,
   easing: "linear",
+});
+
+anime({
+  targets: ".show",
+  translateX: 270,
+  delay: anime.stagger(100), // increase delay by 100ms for each elements.
 });
