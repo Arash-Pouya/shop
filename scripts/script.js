fc2cdea6eb8c379256511f0ebcbeadbbbba74fe9
Arash-pouya <arash.pouya.c@gmail.com>
2021-07-03 10:49:44 +0430

    master

--- scripts/script.js
+++ scripts/script.js
@@ -39,3 +39,24 @@ secondForm.addEventListener("submit", (e) => e.preventDefault());
 /* ///////////details page///////// */
 
 /* ///////////details page///////// */
+(function ($) {
+  $("#thumbcarousel").carousel(0);
+  var $thumbItems = $("#thumbcarousel .item");
+  $("#carousel").on("slide.bs.carousel", function (event) {
+    var $slide = $(event.relatedTarget);
+    var thumbIndex = $slide.data("thumb");
+    var curThumbIndex = $thumbItems.index($thumbItems.filter(".active").get(0));
+    if (curThumbIndex > thumbIndex) {
+      $("#thumbcarousel").one("slid.bs.carousel", function (event) {
+        $("#thumbcarousel").carousel(thumbIndex);
+      });
+      if (curThumbIndex === $thumbItems.length - 1) {
+        $("#thumbcarousel").carousel("next");
+      } else {
+        $("#thumbcarousel").carousel(numThumbItems - 1);
+      }
+    } else {
+      $("#thumbcarousel").carousel(thumbIndex);
+    }
+  });
+})(jQuery);
